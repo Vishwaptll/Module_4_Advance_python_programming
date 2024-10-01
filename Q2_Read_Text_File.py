77b00b9ef705e70d4291fdf325505c247e9506db
Vishwa patel <160208245+Vishwaptll@users.noreply.github.com>
2024-10-01 00:27:04 -0700

    Add files via upload

---
+++ Q2_Read_Text_File.py
@@ -0,0 +1,6 @@
+#2.Write a Python program to read an entire text file.
+
+file=open("m4text.txt","r")
+print(file.read())
+
+file.close
---
+++ Q3_Append_text_File.py
@@ -0,0 +1,6 @@
+#3.Write a Python program to append text to a file and display the text
+
+file=open("m4text.txts","a")
+file.write("This file is now appended")
+
+file.close()
